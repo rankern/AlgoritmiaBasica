a4be0d38bed479b0469d7688fdbf678c7e3a2656
josman231 <josman.romero@gmail.com>
2019-04-17 11:24:59 +0200

    generadores de pruebas actualizados

--- practica1/pruebas/genFichero2.sh
+++ practica1/pruebas/genFichero2.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 aImprimir=0;
-
+if [ -f $1 ]
 for i in $(seq 1 256)
 do 
 	for j in $(seq 1 1000)
 	do
 		echo "$i" >> $1
 	done
+
 done
+fi
+
+exit 1
--- practica1/pruebas/genFichero3.sh
+++ practica1/pruebas/genFichero3.sh
@@ -2,10 +2,15 @@
 
 aImprimir=' ';
 aImprimiAux=$aImprimir;
-for i in $(seq 1 256)
+if [ -f $1 ]
+then
+for i in $(seq 32 256)
 do 
 	for j in $(seq 1 5000)
 	do
 		printf "\\$(printf %o $i)" >>$1
 	done
 done
+fi
+
+exit 1
