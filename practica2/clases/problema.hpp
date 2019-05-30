1598b24d14bf0ef4f340a6b8cbd4553ba1de9a95
rankern <victorm.lafuente@gmail.com>
2019-05-30 03:02:16 +0200

    primera posible version

--- practica2/clases/problema.hpp
+++ practica2/clases/problema.hpp
@@ -1,29 +1,39 @@
 #pragma once
 #include <iostream>
-#include "pedido.hpp";
-using namespace std;
+#include "pedido.hpp"
 
+
+using namespace std;
 class Problema{
+	private:
 	int maxCapacidad = 0;
 	int numEstaciones = 0;
 	int numPedidos = 0;
 	int beneficioAcum = 0;
 	int pedidoActual = 0;
-	int tramos[7] = {0,0,0,0,0,0,0};	
-	bool pedidos[22];
+	int tramos[7] = {0,0,0,0,0,0,0};
 	public:
 		Problema(){}
 		Problema(int _cap, int _estaciones, int _pedidos)
 			: maxCapacidad(_cap), numEstaciones(_estaciones), numPedidos(_pedidos){}
+
+		Problema(int _cap, int _estaciones, int _pedidos, int _pedidoActual, int _beneficioAcum, int _tramos[])
+			: maxCapacidad(_cap), numEstaciones(_estaciones), numPedidos(_pedidos), 
+				pedidoActual(_pedidoActual), beneficioAcum(_beneficioAcum){
+					for(int i = 0; i < 7; i++){
+						tramos[i] = _tramos[i];
+					}
+				}	
 		
 		Problema clone(){
-			return Problema(maxCapacidad, numEstaciones, numPedidos);
+			pedidoActual++;
+			return Problema(maxCapacidad, numEstaciones, numPedidos, pedidoActual-1, beneficioAcum, tramos);
 		}
 		
 		int beneficioActual(){return beneficioAcum;}
 
 		bool anyadir(Pedido &p){
-			pedidos[pedidoActual] = true;
+			this->pedidoActual++;
 			bool ok = true;
 			for(int i = p.estacionIni();i < p.estacionFin() && ok; i++){
 				tramos[i] += p.billetes();
@@ -32,14 +42,107 @@ class Problema{
 			beneficioAcum += p.beneficioPedido();
 			return ok;
 		}
-
-		double costeEstimado(){
-			return beneficioActual();
+		/** indice del vector tramos con mayor cardinal de gente*/
+		int max(int replica[],int desde, int hasta){
+			int mGente = replica[desde];
+			int indice = desde;
+			for(int i = desde; i < hasta; i++){
+				if(replica[i] > mGente){
+					mGente = replica[i];
+					indice = i;
+				}
+			}
+			return indice;
 		}
 
-		int cota_mejor(){
-			return 0;
+		/* void costeYcota(Pedido pedidos[], int numPedidos, double &cota, double &coste) {
+			cota = this->beneficioAcum;
+			coste = this->beneficioAcum;
+			int replicaCota[7] = {this->tramos[0],this->tramos[1],this->tramos[2],this->tramos[3],this->tramos[4],this->tramos[5],this->tramos[6] };
+			int replicaCoste[7] = {this->tramos[0],this->tramos[1],this->tramos[2],this->tramos[3],this->tramos[4],this->tramos[5],this->tramos[6] };
+			for(int i = this->pedidoActual; i < numPedidos; i++){
+				int maxCoste = replicaCoste[pedidos[i].estacionIni()];
+				int maxCota = replicaCota[pedidos[i].estacionIni()];
+				bool viableCota = true;
+				//Calcular tramo ocurrente con mayor num pasajeros
+				for(int j = pedidos[i].estacionIni()+1; j < pedidos[i].estacionFin(); j++){
+					if(replicaCoste[j] > maxCoste)
+						maxCoste = replicaCoste[j];
+					viableCota &= (this->maxCapacidad - replicaCota[j] <= pedidos[i].billetes());
+				}
+				int nuevosPasajeros = pedidos[i].billetes();
+				if(maxCoste < nuevosPasajeros){
+					int nuevosPasajeros =  this->maxCapacidad - maxCoste;
+					
+				}
+				coste = coste + (nuevosPasajeros * (pedidos[i].estacionFin() - pedidos[i].estacionIni()));
+				for(int j = pedidos[i].estacionIni(); j < pedidos[i].estacionFin(); j++){
+					replicaCoste[j] += nuevosPasajeros;
+				}
+				if(viableCota){
+					cota += pedidos[i].beneficioPedido();
+					for(int j = pedidos[i].estacionIni(); j < pedidos[i].estacionFin(); j++){
+						replicaCota[j] += pedidos[i].billetes();
+					}
+				}
+
+			}
+			cota = 0 - cota;
+			coste = 0 - coste;
+		} */
+
+
+		double costeEstimado(Pedido pedidos[], int numPedidos){
+			double coste = this->beneficioAcum;
+			int replica[7] = {this->tramos[0],this->tramos[1],this->tramos[2],this->tramos[3],this->tramos[4],this->tramos[5],this->tramos[6] };
+			for(int i = this->pedidoActual; i < numPedidos; i++){
+				int maxPasajeros = replica[this->max(replica, pedidos[i].estacionIni(), pedidos[i].estacionFin())];
+
+				int nuevosPasajeros = pedidos[i].billetes();
+				int beneficio = pedidos[i].beneficioPedido();
+				if(maxPasajeros < nuevosPasajeros){
+					int nuevosPasajeros =  this->maxCapacidad - maxPasajeros;
+					beneficio = nuevosPasajeros * (pedidos[i].estacionFin() - pedidos[i].estacionIni());
+				}
+				coste = coste + beneficio;
+				for(int j = pedidos[i].estacionIni(); j < pedidos[i].estacionFin(); j++){
+					replica[j] += nuevosPasajeros;
+				}
+			}
+			return 0 - coste;
 		}
 
+		int cota_mejor(Pedido pedidos[], int numPedidos){
+			int cota = this->beneficioAcum;
+			int replica[7] = {this->tramos[0],this->tramos[1],this->tramos[2],this->tramos[3],this->tramos[4],this->tramos[5],this->tramos[6] };
+			for(int i = this->pedidoActual; i < numPedidos; i++){
+				if(replica[this->max(replica,pedidos[i].estacionIni(), pedidos[i].estacionFin())] < pedidos[i].billetes()){
+					continue;
+				}
+				for(int j = pedidos[i].estacionIni(); j < pedidos[i].estacionFin(); j++){
+					replica[j] += pedidos[i].billetes();
+				}
+				cota += pedidos[i].billetes();
+			}
+			return 0 - cota;
+		}
 
+		void incrementarPedido(){pedidoActual++;}
+
+		int siguientePedido(){return this->pedidoActual;}
+
+		bool operator >=(Problema& p) {
+			return this->beneficioActual() >= p.beneficioActual();
+		}
+
+		bool operator <=(Problema& p) {
+			return this->beneficioActual() <= p.beneficioActual();
+		}
+		bool operator >(Problema& p) {
+			return this->beneficioActual() > p.beneficioActual();
+		}
+
+		bool operator <(Problema& p) {
+			return this->beneficioActual() < p.beneficioActual();
+		}		
 };
--- practica2/main.cpp
+++ practica2/main.cpp
@@ -1,24 +1,56 @@
 #include "iostream"
-#include <stdio.h>      /* printf, NULL */
-#include <stdlib.h>     /* srand, rand */
-#include <time.h>       /* time */
+#include <stdio.h>  /* printf, NULL */
+#include <stdlib.h> /* srand, rand */
+#include <time.h>   /* time */
 #include <fstream>
 #include "clases/heap.hpp"
 #include "clases/pedido.hpp"
 #include "clases/problema.hpp"
 using namespace std;
 
-void resolverProblema(Problema &tren, Pedido pedidos[] ){
-
+void resolverProblema(Pedido pedidos[], Heap<Problema> &nodos, int numPedidos){
+	double U_min = 0;
+	int mejorBenefico = 0;
+	while (!nodos.isEmpty()){
+		Problema *noCogido = nodos.pop();
+		Problema cogido = noCogido->clone(); //clone incrementa en 1 el numPedido de noCogido
+		if(cogido.anyadir(pedidos[noCogido->siguientePedido])){
+			double coste = cogido.costeEstimado(pedidos, numPedidos);
+			if(U_min >= coste){
+				int cota = cogido.cota_mejor(pedidos, numPedidos);
+				if((cota == coste || cogido.siguientePedido() == numPedidos) && (0-cota) > mejorBenefico){
+					/** HOJA*/
+					mejorBenefico = 0-cota;
+				}else{
+					nodos.add(&cogido);
+				}
+				if(cota < U_min){
+					U_min = cota;
+				}
+			}
+			 coste = noCogido->costeEstimado(pedidos, numPedidos);
+			if(U_min >= coste){
+				int cota = noCogido->cota_mejor(pedidos, numPedidos);
+				if((cota == coste || noCogido->siguientePedido() == numPedidos) && (0-cota) > mejorBenefico){
+					/** HOJA*/
+					mejorBenefico = 0-cota;
+				}else{
+					nodos.add(noCogido);
+				}
+				if(cota < U_min){
+					U_min = cota;
+				}
+			}
+		}
+	}
 }
 
-
 int main(int argc, char *argv[]){
-	string fichEnt="pruebas.txt";
-	string fichSal="fiche.txt";
+	string fichEnt = "pruebas.txt";
+	string fichSal = "fiche.txt";
 	ifstream fEnt;
 	ofstream fSal;
-	int beneficioMax=0;//U 
+	int beneficioMax = 0; //U
 	int capMax;
 	int numPedidos;
 	int numEstacionFinal;
@@ -26,52 +58,46 @@ int main(int argc, char *argv[]){
 	clock_t start, end;
 
 	fEnt.open(fichEnt);
-	
-	if(fEnt.is_open()){
+
+	if (fEnt.is_open()){
 		fSal.open(fichSal);
-			if(fSal.is_open()){
-		
-		int estSalida;
-		int estLlegada;
-		int numPasajeros;
-		fEnt>>capMax>>numEstacionFinal>>numPedidos;
-		cout<<capMax<<numEstacionFinal<<numPedidos<<endl;
-		Problema tren(capMax, numEstacionFinal, numPedidos);
-		Pedido pedidos[numPedidos];
-		while(!fEnt.eof()){
-			if(capMax!=0 && numEstacionFinal!=0 && numPedidos!=0){
-				start = clock();
-				pedidosLeidos=0;
-				while(pedidosLeidos<numPedidos){
-					
-					fEnt>>estSalida>>estLlegada>>numPasajeros;
-					cout<<estSalida<<estLlegada<<numPasajeros<<endl;
-					pedidos[pedidosLeidos] = Pedido(numPasajeros, estSalida, estLlegada);
-					pedidosLeidos++;
-					//Añadir a montículo
-					//crear Nodo()
+		if (fSal.is_open()){
 
+			int estSalida;
+			int estLlegada;
+			int numPasajeros;
+			fEnt >> capMax >> numEstacionFinal >> numPedidos;
+			cout << capMax << numEstacionFinal << numPedidos << endl;
+			Problema tren(capMax, numEstacionFinal, numPedidos);
+			Pedido pedidos[numPedidos];
+			Heap<Problema> frontera;
+			frontera.add(&tren);
+			while (!fEnt.eof()){
+				if (capMax != 0 && numEstacionFinal != 0 && numPedidos != 0){
+					start = clock();
+					pedidosLeidos = 0;
+					while (pedidosLeidos < numPedidos){
+
+						fEnt >> estSalida >> estLlegada >> numPasajeros;
+						cout << estSalida << estLlegada << numPasajeros << endl;
+						pedidos[pedidosLeidos] = Pedido(numPasajeros, estSalida, estLlegada);
+						pedidosLeidos++;
+					}
+					resolverProblema(pedidos, frontera, numPedidos);
+					end = clock();
+					int tiempo = (end - start);
+					fSal << beneficioMax << " " << tiempo << "\n";
 				}
-				end= clock(); 
-				int tiempo=(end - start);
-				fSal<<beneficioMax<<" "<<tiempo<<"\n";
-			}
 				//Leer otro bloque
-			fEnt>>capMax>>numEstacionFinal>>numPedidos;
-
-		}
-
-		  fSal.close();
+				fEnt >> capMax >> numEstacionFinal >> numPedidos;
 			}
-			else{
-				cout<<"No se ha podido abrir el fichero de salida"<<endl;
-			}
-		  fEnt.close();
+			fSal.close();
+		}else{
+			cout << "No se ha podido abrir el fichero de salida" << endl;
 		}
-	
-
-	else{
-		cout<<"No se ha podido abrir el fichero de entrada"<<endl;
+		fEnt.close();
+	}else{
+		cout << "No se ha podido abrir el fichero de entrada" << endl;
 	}
 
 	return 0;
