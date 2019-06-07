aee1ee7847bdf7f20f2159d4f8bbfe3b877d906a
josman231 <josman.romero@gmail.com>
2019-06-07 20:59:41 +0200

    Update heap.hpp

--- practica2/clases/heap.hpp
+++ practica2/clases/heap.hpp
@@ -1,6 +1,6 @@
 /**
- * @autor: Victor M. Lafuente
- * @autor: Jose M. Romero
+ * @autor: Victor M. Lafuente NIA:747325
+ * @autor: Jose M. Romero NIA:740914
  * @fecha: 06-03-2019
  * @ultima Modificacion: 13-03-2019
  */ 
@@ -9,18 +9,32 @@
 #include <iostream>
 using namespace std;
 
-const int MAX = 100000;
+const int MAX = 100000;//capacidad máxima del montículo
 
 template <typename T>
+//Clase montículo
 class Heap{
-	int tam = 0;
-	T * elementos[MAX];
+	int tam = 0; //tamaño del montículo
+	T * elementos[MAX]; //Vector de elementos
 
 	public:
+		//Crea un montículo vacío
 		Heap() : tam (0){}
 
+
+		/*Pre:
+		*Post:Devuelve true si el montículo está vacío
+		*/
 		bool isEmpty(){ return this->tam == 0; }
+
+		/*Pre:
+		*Post:Devuelve el número de elemento en el montículo
+		*/
 		int tamanyo() {return this->tam;}
+
+		/*Pre:
+		*Post:Añade al montículo el elemento e
+		*/
 		void add(T * e){
 			this->elementos[this->tam] = e;
 			for (int i = this->tam++; i > 0 && *this->elementos[i] < *this->elementos[(i-1)/2]; i = (i-1)/2){
@@ -29,17 +43,11 @@ class Heap{
 				this->elementos[(i-1)/2] = aux;
 			}
 		}
-
+		/*Pre:
+		 *Post:Ha quitado el último elemento del montículo
+		 */
 
 		T * pop(){
-			/*for (int i = 0; i< this->tam; i++){
-				if(this->elementos[i]->getIzq() == nullptr){
-					printf("(%c, %d)", this->elementos[i]->getElement(), this->elementos[i]->getFrecuencia());
-				}else{
-					printf("(N, %d)", this->elementos[i]->getFrecuencia());
-				}
-			}
-			cout << endl;*/
 			T * ret = this->elementos[0];
 			this->tam--;
 			this->elementos[0] = this->elementos[this->tam];
